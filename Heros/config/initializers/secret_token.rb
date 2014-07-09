1cd8838f79c5725a8443a78f4e2a282e2d70e3f5
Evelim Carla Ribeiro <ecr10@inf.ufpr.br>
2014-07-09 02:04:26 -0300

    first commit

---
+++ Heros/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Heros::Application.config.secret_key_base = '523df57b28104ba7f02cc0ec428d103fef04abb1330e7f37d786af494878f01f491a5baafeb2cb7d8708ba3fe2a1858c07685dd2e0858ad205947c97845a276c'
---
+++ Heros/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Heros::Application.config.session_store :cookie_store, key: '_Heros_session'
