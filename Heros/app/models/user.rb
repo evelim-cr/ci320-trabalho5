c6382bce7bff6e22411d08a80b716cff0a453822
Evelim Carla Ribeiro <ecr10@inf.ufpr.br>
2014-07-10 00:54:50 -0300

    created sessions, log out, and css modified
    Signed-off-by: Evelim Carla Ribeiro <ecr10@inf.ufpr.br>

--- Heros/app/models/user.rb
+++ Heros/app/models/user.rb
@@ -13,5 +13,14 @@ class User < ActiveRecord::Base
 	      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)  
 	    end
  	end	
+
+ 	def self.authenticate(login, password)  
+    	user = find_by_login(login)  
+	    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt) 
+	      user  
+	    else  
+	      nil  
+	    end  
+  	end  
 end
 
